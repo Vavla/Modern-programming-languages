c81cd57ead4efeed63183715fb15bca84d1e1611
Vavla <vlasovalera05@gmail.com>
2025-10-02 10:20:49 +1000

    Add generate files

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,45 +1,20 @@
 import csv
 import random
+from concurrent.futures import ProcessPoolExecutor as Pool
+import pandas as pd
 
 FILENAME1 = "file1.csv"
-
-
+FILENAME2 = "file2.csv"
+FILENAME3 = "file3.csv"
+FILENAME4 = "file4.csv"
+FILENAME5 = "file5.csv"
+filenames = [FILENAME1,FILENAME2,FILENAME3,FILENAME4,FILENAME5]
 categories = "ABCD"
 
- 
-with open(FILENAME1, "w", newline="") as file:
-    writer = csv.writer(file)
-    for i in range(0,20):
-        stroka = [random.choice(categories),round(random.random()*100,2)]
-        writer.writerow(stroka)
-with open(FILENAME2, "w", newline="") as file:
-    writer = csv.writer(file)
-    for i in range(0,20):
-        stroka = [random.choice(categories),round(random.random()*100,2)]
-        writer.writerow(stroka)
-with open(FILENAME3, "w", newline="") as file:
-    writer = csv.writer(file)
-    for i in range(0,20):
-        stroka = [random.choice(categories),round(random.random()*100,2)]
-        writer.writerow(stroka)
-with open(FILENAME4, "w", newline="") as file:
-    writer = csv.writer(file)
-    for i in range(0,20):
-        stroka = [random.choice(categories),round(random.random()*100,2)]
-        writer.writerow(stroka)
-with open(FILENAME5, "w", newline="") as file:
-    writer = csv.writer(file)
-    for i in range(0,20):
-        stroka = [random.choice(categories),round(random.random()*100,2)]
-        writer.writerow(stroka)  
+for fname in filenames:
+    with open(fname, "w", newline="") as file:
+        writer = csv.writer(file)
+        for i in range(0,20):
+            stroka = [random.choice(categories),round(random.random()*100,2)]
+            writer.writerow(stroka)
 
-
-#считали данные, сразу записыаем в нужную строку матрицы, в зависимости от номера буквы. Далее по каждой строке поиск медианы, вывод с буквой. потом будет общая матрица со всеми данными 
-def medianList(array1):
-    array1 = sorted(array1)
-    if((len(array1) % 2) != 0):
-         return array1[len(array1)/2]
-    else:
-        return (array1[len(array1)//2 + 1] + array1[len(array1)//2])/2
-
-#print(medianList())
