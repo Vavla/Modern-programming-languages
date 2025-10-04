edaf0e11fd3243e0e2e60424876354b733c4a57f
Vavla <vlasovalera05@gmail.com>
2025-10-04 10:01:15 +1000

    Add final search median and fix processing of data

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,6 +3,7 @@ import random
 from concurrent.futures import ProcessPoolExecutor as Pool
 import pandas as pd
 import numpy as np
+import time
 
 FILENAME1 = "file1.csv"
 FILENAME2 = "file2.csv"
@@ -11,6 +12,7 @@ FILENAME4 = "file4.csv"
 FILENAME5 = "file5.csv"
 filenames = [FILENAME1,FILENAME2,FILENAME3,FILENAME4,FILENAME5]
 categories = "ABCD"
+
 
 for fname in filenames:
     with open(fname, "w", newline="") as file:
@@ -29,6 +31,7 @@ def median_list(array1):
 
 
 def processing_file(fname):
+    time.sleep(1)
     array_letter_a = []
     array_letter_b = []
     array_letter_c = []
@@ -48,22 +51,30 @@ def processing_file(fname):
                 array_letter_d.append(float(row[1]))
     with open(f'{fname[0:5]}_out.txt', 'w') as m_file:
         if(len(array_letter_a) != 0):
-            m_file.write('A: '+ str(median_list(array_letter_a))+'\n')
+            m_file.write('A: '+ str(median_list(array_letter_a)) + f" {np.std(array_letter_a)}"+'\n')
         if(len(array_letter_b) != 0):
-            m_file.write('B: '+ str(median_list(array_letter_b))+'\n')
-            print(fname,'B: '+ median_list(array_letter_b))
+            m_file.write('B: '+ str(median_list(array_letter_b))+ f" {np.std(array_letter_b)}"+'\n')
         if(len(array_letter_c) != 0):
-            m_file.write('C: ' + str(median_list(array_letter_c))+'\n')
-            print(fname,'C: '+ str(median_list(array_letter_c)))
+            m_file.write('C: ' + str(median_list(array_letter_c))+ f" {np.std(array_letter_c)}"+'\n')
         if(len(array_letter_d) != 0):
-            m_file.write('D: ' + str(median_list(array_letter_d))+'\n')
-            print(fname,'D: '+ str(median_list(array_letter_d)))
+            m_file.write('D: ' + str(median_list(array_letter_d))+ f" {np.std(array_letter_d)}"+'\n')
     
+def final_search_median():
+    array_letter_final = [[],[],[],[]]
+    for i in filenames:
+        with open(f'{i[0:5]}_out.txt', 'r') as m_file:
+            reader = m_file.readlines()
+            for i in range(4):
+                array_letter_final[i].append(float((reader[i][3:-2:1].split(' ')[0])))
+    for i in range(4):
+        print(f"Final {chr(65+i)}: [{array_letter_final[i]}] {median_list(array_letter_final[i])} {np.std(array_letter_final[i])}\n")
 
 if __name__ == "__main__":
     with Pool(5) as executor:
     # Запуск задачи параллельно
         executor.map(processing_file, filenames)
-    # Ожидание результата и получение его
+    
+    final_search_median()
+
     
 
