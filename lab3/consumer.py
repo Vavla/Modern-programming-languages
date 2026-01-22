a1012c0699a9b0e17fc673e7bf4fc12858f35fd4
Vavla <vlasovalera05@gmail.com>
2026-01-22 20:35:23 +1000

    feat: add deserialization data

--- lab3/consumer.py
+++ lab3/consumer.py
@@ -1,1 +1,5 @@
+from kafka import KafkaConsumer
+import json
 
+def deserialization(serialized): #полученные из топика данные десериализуем (байты -> начальный формат)
+    return json.loads(serialized)
